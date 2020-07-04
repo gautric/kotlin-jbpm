7538a1abee395389523bb0b0a20fadd264ef37a8
admin <gautric@redhat.com>
2020-07-04 11:53:55 +0200

    Cleanning Security

--- src/main/kotlin/net/a/g/jbpm/service/DefaultWebSecurityConfig.kt
+++ src/main/kotlin/net/a/g/jbpm/service/DefaultWebSecurityConfig.kt
@@ -1,34 +1,55 @@
 package net.a.g.jbpm.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
 
 @EnableWebSecurity
 @Order(1)
 class DefaultWebSecurityConfig : WebSecurityConfigurerAdapter() {
-    
-    @Throws(java.lang.Exception::class)
+
     override fun configure(http: HttpSecurity) {
-        http
-                .cors().and()
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/rest/*").authenticated().and()
-                .httpBasic().and()
-                .headers().frameOptions().disable()
+        http.cors().and()
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/rest/*").authenticated().and()
+            .httpBasic().and()
+            .headers().frameOptions().disable()
     }
 
-
     @Autowired
-    @Throws(Exception::class)
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("kie-server")
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("admin")
-        auth.inMemoryAuthentication().withUser("kieserver").password("{noop}kieserver123_").roles("kie-server")
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("admin")
+        auth.inMemoryAuthentication().withUser("kieserver").password("kieserver123_").roles("kie-server")
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val source = UrlBasedCorsConfigurationSource()
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowedOrigins = Arrays.asList("*")
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.allowedMethods = Arrays.asList(HttpMethod.GET.name, HttpMethod.HEAD.name,
+                HttpMethod.POST.name, HttpMethod.DELETE.name, HttpMethod.PUT.name)
+        corsConfiguration.applyPermitDefaultValues()
+        source.registerCorsConfiguration("/**", corsConfiguration)
+        return source
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder? {
+        return NoOpPasswordEncoder.getInstance()
     }
 }
 
